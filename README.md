7746254475180ea57bd6c42dbab4480eca7007d3
Nyi Nyi Lwin <mdy.nnl@gmail.com>
2024-07-06 07:26:06 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Stack
+
+- Based on [Remix Blues Stack](https://github.com/remix-run/blues-stack)
+- Email/Password Authentication with [cookie-based sessions](https://remix.run/utils/sessions#creatememorysessionstorage)
+- Database ORM with [Prisma](https://prisma.io) with PostgreSQL
+- Styling with [Tailwind](https://tailwindcss.com/)
+- Unit testing with [Vitest](https://vitest.dev) and [Testing Library](https://testing-library.com)
+- Code formatting with [Prettier](https://prettier.io)
+- Linting with [ESLint](https://eslint.org)
+- Static Types with [TypeScript](https://typescriptlang.org)
---
+++ app/root.tsx
@@ -0,0 +1,37 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import { getUser } from "~/session.server";
+import "./tailwind.css";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  return json({ user: await getUser(request) });
+};
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className="h-full">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-full">
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return <Outlet />;
+}
